17d35b0db4cec50f18c13ab39afd2a603f2294a2
METAORI <nico97.mecca@gmail.com>
2024-06-17 18:10:59 +0200

    introduzione js

--- Unit 1/Javascript 1/introudzione js/D1.js
+++ Unit 1/Javascript 1/introudzione js/D1.js
@@ -14,58 +14,64 @@ REGOLE
  dichiarare una variabile che ancora però non ha un valore ma le verrà assegnata in futuro)
 */
 
-let myName= 'Nicolò'
-console.log(myName)
+let myName= 'Nicolò';
+console.log(myName);
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-let numero1=12
-let numero2=20
-let somma= numero1 + numero2
-console.log(somma)
+let numero1=12;
+let numero2=20;
+let somma= numero1 + numero2;
+console.log(somma);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-let x=12
-console.log(x)
+let x=12;
+console.log(x);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-myName='Mecca'
+myName='Mecca';
+console.log(myName);
 
-Const age=24
-Const age=26
-console.log(age)
+const age=24;
+console.log(age);
 
+//genera un errore
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-let numero=4
-let sottrazione= x - numero
-console.log(sottrazione)
+let numero=4;
+let sottrazione= x - numero;
+console.log(sottrazione);
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-let name1='jhon'
-let name2='Jhon'
-let verifica= name1===name2?'la verifica è true':'la verifica è false'
-console.log(verifica)
+let name1='jhon';
+let name2='Jhon';
+const verifica= 
+name1!==name2?'name1  è diverso da name2':'name1 è uguale da name2';
+console.log(verifica);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
-let trasformazione= name2.toLowerCase()===name1.toLowerCase()?'true':'false'
-console.log(trasformazione)
+
+name1= name1.toLowerCase();
+name2= name1.toLowerCase();
+const verifica2=
+name1!==name2?'name1 è diverso da name2':'name1 è uguale a name2'
+console.log(verifica2)
