12e1e7e2a49f6e1133ce99b387b1c82b0f40a485
METAORI <nico97.mecca@gmail.com>
2024-07-25 22:01:20 +0200

    U2 W3 D4

--- Unit 1/Javascript III/U2 W3 D4/fetch.js
+++ Unit 1/Javascript III/U2 W3 D4/fetch.js
@@ -1,71 +1,40 @@
 const apiKey = 'Mwr68UJ5Wpe2apOFidE1MyQRRkAPJktMKvlBbGhL0ZFu8uSLHy39Hye4'; 
-const imageContainer = document.getElementById('imagePexels');
-const loadImagesBtn = document.getElementById('loadImages');
-const loadSecondaryImagesBtn = document.getElementById('loadSecondaryImages');
-const searchInput = document.getElementById('searchQuery');
 
-const generateImage = function () {
-    fetch('https://api.pexels.com/v1/search?query=wolf', {
-        headers: {
-            Authorization: apiKey
-        }
-    })
-    .then((response) => {
-        if (response.ok) {
-            
-            return response.json();
-        } else {
-            throw new Error('Network response was not ok');
-        }
-    })
-    .then((imageData)=>{
-        console.log(imageData)
-        imageData.forEach((img)=>{
-            const newImage = `
-                 <div class="col-md-4">
-              <div class="card mb-4 shadow-sm">
-                <img
-                  src="${photo.src.medium}"
-                  class="bd-placeholder-img card-img-top"
-                />
-                <div class="card-body">
-                  <h5 class="card-title">${photo.alt}</h5>
-                  <p class="card-text">
-                  ${photo.description || 'No description available.'}
-                  </p>
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div class="btn-group">
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        View
-                      </button>
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        Edit
-                      </button>
-                    </div>
-                    <small class="text-muted">${photo.id}</small>
-                  </div>
-                </div>
-              </div>
-            </div> `
-        })
-    })
-    .catch((error) => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
-};
-
-generateImage()
-
-function hideCard(button) {
-    const card = button.closest('.col-md-4');
-    card.style.display = 'none'; // Hide the card
+const imageData = function(image) {
+fetch(`https://api.pexels.com/v1/search?query=${image}`,{
+  headers:{
+    authorization: apiKey,
+  },
+})
+.then((response)=> {
+if(response.ok){
+  return response.json()
+}else{
+  throw new Error('qualcosa non va')
 }
+})
+.then((data)=> {
+console.log('Data Img',data)
+const actualPicture = document.querySelectorAll('.card-img-top')
+console.log(actualPicture)
+for (let i = 0; i < actualPicture.length; i++) {
+  // per OGNI immagine di un cane vado a sostituire la sua "src"
+  // con quella di UNA delle foto di pexels
+  actualPicture[i].src = data.photos[i].src.medium
+}
+})
+.catch((err) => {
+  console.log('error', err)
+})
+}
+const loadButton = document.querySelector('.btn-primary').addEventListener('click', function () {
+  imageData('esoterico')})
 
+const secondLoadButton = document.querySelector('.btn-secondary').addEventListener('click', function () {
+    imageData('camper')})
+
+
+const thirdButton = document.querySelector('.btn-info').addEventListener('click',function(){
+const valueInput = document.getElementById('search').value
+imageData(valueInput)
+})
